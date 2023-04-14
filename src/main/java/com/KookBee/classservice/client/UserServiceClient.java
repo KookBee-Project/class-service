3cad384684d713b87ea2208b105868f4aad5b4df
laon0125 <jinu0125@gmail.com>
2023-04-14 09:47:26 +0900

    #1_230414_1 feature/Manager/CreateClass/bugFix

---
+++ src/main/java/com/KookBee/classservice/client/UserServiceClient.java
@@ -0,0 +1,12 @@
+package com.KookBee.classservice.client;
+
+import org.apache.catalina.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "USER-SERVICE")
+public interface UserServiceClient {
+    @GetMapping("/user/{userId}")
+    User getUserById(@PathVariable("userId") Long userId);
+}
