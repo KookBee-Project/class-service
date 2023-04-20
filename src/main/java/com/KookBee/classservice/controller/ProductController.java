f3fde3e8d346b236c35090a1756a4e9449cb7220
pshoon25 <pshoon25@naver.com>
2023-04-20 16:46:13 +0900

    #3_230420_2 feature/Student/Product

--- src/main/java/com/KookBee/classservice/controller/ProductController.java
+++ src/main/java/com/KookBee/classservice/controller/ProductController.java
@@ -1,12 +1,13 @@
 package com.KookBee.classservice.controller;
 
 import com.KookBee.classservice.domain.request.ProductRequest;
+import com.KookBee.classservice.domain.response.OfferProductResponse;
+import com.KookBee.classservice.domain.response.RentalProductResponse;
 import com.KookBee.classservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -16,5 +17,18 @@ public class ProductController {
     @PostMapping
     public String postProduct(@RequestBody ProductRequest productRequest){
         return productService.postProductService(productRequest);
+    }
+
+
+
+    // Student 물품 제공 내역 조회
+    @GetMapping("/offerproduct")
+    public List<OfferProductResponse> getOfferProduct(){
+        return productService.getOfferProductService();
+    }
+    // Student 물품 대여 내역 조회
+    @GetMapping("/rentalproduct")
+    public List<RentalProductResponse> getRentalProduct(){
+        return productService.getRentalProductService();
     }
 }
--- src/main/java/com/KookBee/classservice/service/ProductService.java
+++ src/main/java/com/KookBee/classservice/service/ProductService.java
@@ -2,12 +2,19 @@ package com.KookBee.classservice.service;
 
 
 import com.KookBee.classservice.domain.entity.Product;
+import com.KookBee.classservice.domain.enums.EProductType;
 import com.KookBee.classservice.domain.request.ProductRequest;
+import com.KookBee.classservice.domain.response.OfferProductResponse;
+import com.KookBee.classservice.domain.response.RentalProductResponse;
 import com.KookBee.classservice.repository.ProductRepository;
 import com.KookBee.classservice.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +22,8 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
     private final JwtService jwtService;
+
+    // Manager 물품 대여 및 제공 서비스 등록
     public String postProductService(ProductRequest productRequest) {
         Long managerId = jwtService.tokenToDTO(jwtService.getAccessToken()).getId();
         productRequest.setManagerId(managerId);
@@ -25,5 +34,23 @@ public class ProductService {
         } else {
             throw new RuntimeException("등록에 실패하였습니다.");
         }
+    }
+
+    // Student 물품 대여 및 제공 서비스 조회
+    public List<OfferProductResponse> getOfferProductService(){
+        Long studentId = jwtService.tokenToDTO(jwtService.getAccessToken()).getId();
+        List<Product> product = productRepository.findByStudentIdAndProductType(studentId, EProductType.OFFER);
+        List<OfferProductResponse> responses = product.stream()
+                .map(OfferProductResponse::new)
+                .collect(Collectors.toList());
+        return responses;
+    }
+    public List<RentalProductResponse> getRentalProductService(){
+        Long studentId = jwtService.tokenToDTO(jwtService.getAccessToken()).getId();
+        List<Product> product = productRepository.findByStudentIdAndProductType(studentId, EProductType.RENTAL);
+        List<RentalProductResponse> responses = product.stream()
+                .map(RentalProductResponse::new)
+                .collect(Collectors.toList());
+        return responses;
     }
 }
